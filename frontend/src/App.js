5e6516f37ea3e8079b22f8fc113636c1b8dbc5c3
Andrew Nguyen <andrew.nguyen.ta@gmail.com>
2024-09-05 23:34:14 -0500

    initialization with dependencies

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'
+
+function App() {
+  return (
+      <div className='bg-slate-500'>
+        Hello World!
+      </div>
+  );
+}
+
+export default App;
