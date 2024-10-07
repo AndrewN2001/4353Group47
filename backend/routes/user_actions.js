f117f1fdf5e170e8cdae3e31f006f0c7511bf7e4
Kevin <kevleon519@gmail.com>
2024-10-07 13:39:30 -0500

    connected user profile to backend

--- backend/routes/user_actions.js
+++ backend/routes/user_actions.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/user_controller');
 
 router.post('/login', userController.handleLogin);
 router.post('/register', userController.handleRegister);
+router.get('/profile/:userID', userController.getUserProfile);
 
 module.exports = router;
