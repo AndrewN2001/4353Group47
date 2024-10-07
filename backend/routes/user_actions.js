05e23cb360f964883af65db48b931b5ff77a7d38
Andrew Nguyen <andrew.nguyen.ta@gmail.com>
2024-10-07 15:56:17 -0500

    created notifications API

--- backend/routes/user_actions.js
+++ backend/routes/user_actions.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/user_controller');
 router.post('/login', userController.handleLogin);
 router.post('/register', userController.handleRegister);
 router.get('/profile/:userID', userController.getUserProfile);
+router.patch('/notifications/:userID', userController.handleNotifications);
 
 module.exports = router;
--- frontend/src/components/notifications.js
+++ frontend/src/components/notifications.js
@@ -1,6 +1,23 @@
 import React, {useState} from "react"
+import axios from "axios";
 
 export default function Notifications(){
+    const [notifications, setNotifications] = useState({
+        newAssignments: true,
+        newUpdates: true,
+        newReminders: true
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(notifications);
+        const userID = "someUserId";
+        axios.post(`http://localhost:3001/api/users/notifications/${userID}`, {notifications})
+        .then(result => {
+            console.log(result);
+        })
+    }
+
     return(
         <div className="p-7 col-span-1 w-fit">
             <h1 className="text-4xl text-nowrap">
@@ -8,39 +25,62 @@ export default function Notifications(){
             </h1>
 
             <div className="mt-10">
-                <ul className="flex flex-col w-full">
-                    <li className="flex justify-between">
+                <form className="flex flex-col w-full">
+                    <div className="flex justify-between">
                         <h1 className="text-xl py-3">
                             New Event Assignments
                         </h1>
-                        
-                        <label class="inline-flex items-center cursor-pointer">
-                            <input type="checkbox" value="" class="sr-only peer"/>
-                            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500"></div>
+                        <label className="inline-flex items-center cursor-pointer">
+                            <input 
+                                type="checkbox" 
+                                value=""
+                                checked={notifications.newAssignments}
+                                className="sr-only peer"
+                                onChange={(e) => setNotifications({...notifications, newAssignments: e.target.checked})}
+                            />
+                            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500"></div>
                         </label>
-                    </li>
-                    <li className="flex justify-between">
+                    </div>
+                    
+                    <div className="flex justify-between">
                         <h1 className="text-xl py-3">
                             New Event Updates
                         </h1>
 
-                        <label class="inline-flex items-center cursor-pointer">
-                            <input type="checkbox" value="" class="sr-only peer"/>
-                            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500"></div>
+                        <label className="inline-flex items-center cursor-pointer">
+                            <input 
+                                type="checkbox" 
+                                value=""
+                                checked={notifications.newUpdates}
+                                className="sr-only peer"
+                                onChange={(e) => setNotifications({...notifications, newUpdates: e.target.checked})}
+                            />
+                            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500"></div>
                         </label>
-                    </li>
-                    <li className="flex justify-between">
+                    </div>
+                    
+                    <div className="flex justify-between">
                         <h1 className="text-xl py-3">
                             New Event Reminders
                         </h1>
 
-                        <label class="inline-flex items-center cursor-pointer">
-                            <input type="checkbox" value="" class="sr-only peer"/>
-                            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500"></div>
+                        <label className="inline-flex items-center cursor-pointer">
+                            <input 
+                                type="checkbox" 
+                                value=""
+                                checked={notifications.newReminders}
+                                className="sr-only peer"
+                                onChange={(e) => setNotifications({...notifications, newReminders: e.target.checked})}
+                            />
+                            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500"></div>
                         </label>
-                    </li>
-                </ul>
+                    </div>
+                </form>
             </div>
+
+            <button className="bg-blue-500 hover:bg-blue-400 text-white py-3 px-10 rounded-md mt-10" onClick={handleSubmit}>
+                <p className="font-bold">Save</p>
+            </button>
         </div>
     )
 }
