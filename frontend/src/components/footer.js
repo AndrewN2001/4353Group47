bd7e803ef245633a5dc96c594a3c9f357b562293
Andrew Nguyen <andrew.nguyen.ta@gmail.com>
2024-10-14 23:00:55 -0500

    minor changes

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer(){
     return(
         <div className="text-center bg-gray-600 text-white font-light py-6">
-            <a className="hover:underline hover:underline-offset-2" target='_blank' href="https://github.com/AndrewN2001/4353Group47">Github</a> | Created by Andrew Nguyen and Kevin Leon for COSC 4353. © 2024
+            <a className="hover:underline hover:underline-offset-2" target='_blank' rel="noreferrer" href="https://github.com/AndrewN2001/4353Group47">Github</a> | Created by Andrew Nguyen and Kevin Leon for COSC 4353. © 2024
         </div>
     )
 }
